n-step bc eval (have bug); exp weighting

advantage.py
@@ -76,12 +76,13 @@ class Advantage(nn.Module):
 	# for 
 	@torch.no_grad()
 	def td(self, data):
+		if self.td_type == 'nstep' and self.replay_buffer.n_step > 1:
+			state, action, state_n, ret_n, done_n = data
+			return ret_n + (1-done_n) * (self.discount**self.n_step) * self.get_value_target(state_n)	
+		
 		state, action, next_state, reward, not_done, dones_float, rets = data
-		if self.td_type == 'nstep':
-			if self.replay_buffer.n_step == 1:
-				return reward + not_done * self.discount * self.get_value_target(next_state)
-			else:
-				raise NotImplementedError
+		if self.td_type == 'nstep' and self.replay_buffer.n_step == 1:
+				return reward + not_done * self.discount * self.get_value_target(next_state)	
 		elif self.td_type == 'mc': # 1. from current timestep t from T; 2. discounted
 			return rets
 		else:
@@ -93,9 +94,28 @@ class Advantage(nn.Module):
 	
 	def set_replay_buffer(self, replay_buffer):
 		self.replay_buffer = replay_buffer
+		self.n_step = replay_buffer.n_step
 
 	@torch.no_grad()
 	def eval(self, batch_size=256):
+		n_step = self.replay_buffer.n_step
+		if self.adv_type == 'nstep' and n_step > 1:
+			ret_n, done_n = self.replay_buffer.ret_n, self.replay_buffer.done_n
+			values, next_values = [], []
+			for l in range(0, self.replay_buffer.size, batch_size):
+				r = min(l+batch_size, self.replay_buffer.size)
+				ind = list(range(l, r))
+				data = self.replay_buffer.sample_n_step_by_ind(ind)
+				state, next_state = data[0], data[2]
+				v0 = self.get_value(state) 
+				v1 = self.get_value(next_state)
+				values.append(v0.cpu())
+				next_values.append(v1.cpu())
+			values, next_values = np.concatenate(values),  np.concatenate(next_values)
+			q = ret_n + (1 - done_n) * (self.discount**n_step) * next_values
+			adv = q - values
+			return adv, q, values
+
 		values, next_values = [], []
 		for l in range(0, self.replay_buffer.size, batch_size):
 			r = min(l+batch_size, self.replay_buffer.size)
@@ -108,12 +128,9 @@ class Advantage(nn.Module):
 		values, next_values = np.concatenate(values),  np.concatenate(next_values)
 		rewards, not_dones, dones_float = self.replay_buffer.reward, self.replay_buffer.not_done, self.replay_buffer.dones_float
 		bs = rewards.shape[0]
-		if self.adv_type == 'nstep':
-			if self.replay_buffer.n_step == 1:
-				q = rewards + not_dones * self.discount * next_values
-				adv = q - values
-			else:
-				raise NotImplementedError
+		if self.adv_type == 'nstep' and n_step == 1:
+			q = rewards + not_dones * self.discount * next_values
+			adv = q - values
 		if self.adv_type == 'gae':
 			adv = np.zeros((bs+1, 1))
 			delta = rewards + not_dones * self.discount * next_values - values 
@@ -150,7 +167,10 @@ class V_Advantage(Advantage):
 	def train(self, replay_buffer):
 		self.total_it += 1
 		# Sample replay buffer 
-		data = replay_buffer.bc_eval_sample()
+		if self.td_type == 'nstep' and self.replay_buffer.n_step > 1:
+			data = replay_buffer.bc_eval_sample_n()
+		else:
+			data = replay_buffer.bc_eval_sample()
 		state = data[0]
 
 		with torch.no_grad():
@@ -213,7 +233,10 @@ class DoubleV_Advantage(V_Advantage):
 	def train(self, replay_buffer):
 		self.total_it += 1
 		# Sample replay buffer 
-		data = replay_buffer.bc_eval_sample()
+		if self.td_type == 'nstep' and self.replay_buffer.n_step > 1:
+			data = replay_buffer.bc_eval_sample_n()
+		else:
+			data = replay_buffer.bc_eval_sample()
 		state = data[0]
 
 		with torch.no_grad():
@@ -277,7 +300,7 @@ class VQ_Advantage(Advantage):
 		self.total_it += 1
 		# Sample replay buffer 
 		data = replay_buffer.bc_eval_sample()
-		state, action, next_state, reward, not_done, returns = data
+		state, action = data[0], data[1]
 
 		with torch.no_grad():
 			v_target = self.td(data)

main.py
@@ -58,6 +58,8 @@ if __name__ == "__main__":
     parser.add_argument("--lambd", type=float, default=0.95, help='gae lambda') 
     parser.add_argument("--bc_lr_schedule", type=str, default='cosine', choices=['cosine', 'linear', 'none']) 
     parser.add_argument("--weight_freq", default=5e4, type=int)    
+    parser.add_argument("--weight_func", default='linear', choices=['linear', 'exp'])    
+    parser.add_argument("--exp_lambd", default=1.0, type=float)    
     # TD3
     parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
     parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
@@ -125,6 +127,8 @@ if __name__ == "__main__":
         "bc_eval": args.bc_eval,
         "bc_eval_steps": args.bc_eval_steps,
         "bc_lr_schedule": args.bc_lr_schedule,
+        "weight_func": args.weight_func,
+        "exp_lambd": args.exp_lambd,
         **adv_kawargs,
         # TD3
         "policy_noise": args.policy_noise * max_action,
@@ -179,7 +183,7 @@ if __name__ == "__main__":
                 wandb.log({f'bc/eval/v': v.mean()}, step=t+1)
                 wandb.log({f'bc/eval/abs_adv': np.abs(adv).mean()}, step=t+1)
                 wandb.log({f'bc/eval/positive_adv': (adv-adv.min()).mean()}, step=t+1)
-        replay_buffer.replace_weights(bc_eval_results[t+1]['adv'])
+        replay_buffer.replace_weights(bc_eval_results[t+1]['adv'], args.weight_func, args.exp_lambd)
         np.save(f'./weights/{file_name}.npy', bc_eval_results)
 
     # Initialize policy

utils.py
@@ -81,6 +81,21 @@ class ReplayBuffer(object):
         ind = np.random.randint(self.size, size=self.batch_size)
         return self.sample_by_ind(ind)
 
+    def sample_n_step_by_ind(self, ind):
+        return (
+            torch.FloatTensor(self.state[ind]).to(self.device),
+            torch.FloatTensor(self.action[ind]).to(self.device),
+            torch.FloatTensor(self.state_n[ind]).to(self.device),
+            torch.FloatTensor(self.ret_n[ind]).to(self.device),
+            torch.FloatTensor(self.done_n[ind]).to(self.device),
+        )
+
+    def bc_eval_sample_n(self):
+        # uniform sample
+        ind = np.random.randint(self.size, size=self.batch_size)
+        return self.sample_n_step_by_ind(ind)
+
+
     # use for training
     def sample(self):
         ind = self.sampler.sample()
@@ -134,6 +149,22 @@ class ReplayBuffer(object):
             self.sampler = PrefetchBalancedSampler(self.probs, self.size, self.batch_size, n_prefetch=1000)
         else:
             self.sampler = RandSampler(self.size, self.batch_size)
+
+        # n-step bootstrap for bc eval
+        if self.n_step == 1: return
+        ret_n = np.copy(self.reward)
+        # done_n = 1 - self.not_done
+        done_n = np.copy(self.dones_float)
+        for n in range(1, self.n_step):
+            # alternatively calculate return_n_step and done_n_step
+            ret_n[:-n] += (self.discount ** n) * self.reward[n:] * (1 - done_n[:-n])
+            done_n[:-n] = np.maximum(done_n[:-n], self.dones_float[n:])
+        # !  While here does not estimate value of the truncated state, we (intuitively) should.
+        state_n = np.zeros_like(self.state)
+        state_n[:-self.n_step+1] = self.next_state[self.n_step-1:]
+        
+        self.ret_n, self.done_n, self.state_n = ret_n, done_n, state_n
+
     
     def compute_return(self):
         returns, ret, start = [], 0, 0
@@ -153,10 +184,15 @@ class ReplayBuffer(object):
         self.next_state = (self.next_state - mean)/std
         return mean, std
 
-    def replace_weights(self, adv):
+    def replace_weights(self, adv, weight_func, exp_lambd=1.0):
         #? need set adv_prob_base?
-        positive_adv = adv - adv.min()
-        prob = positive_adv / positive_adv.sum()
+        if weight_func == 'linear':
+            positive_adv = adv - adv.min()
+            prob = positive_adv / positive_adv.sum()
+        elif weight_func == 'exp':
+            adv = adv / np.abs(adv).mean()
+            positive_adv = np.exp(exp_lambd * adv)
+            prob = positive_adv / positive_adv.sum()
         if self.reweight:
             if len(prob.shape) == 1:
                 prob = np.expand_dims(prob, 1)